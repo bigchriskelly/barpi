982acded6cf93d521fb6bef6e9c8f002a9ee6b0e
bigchriskelly <bigchriskelly@hotmail.com>
2020-07-28 14:12:09 +0100

    updates

--- temp.py
+++ temp.py
@@ -1,18 +1,22 @@
 import glob
 import time
 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+bus_index = 0
+#base_dir = '/sys/bus/w1/devices/'
+#device_folder = glob.glob(base_dir + '28*')[bus_index]
+#device_file = device_folder + '/w1_slave'
 
-def read_temp_raw():
+def read_temp_raw(device_file):
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
 def read_temp():
-    lines = read_temp_raw()
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[bus_index]
+    device_file = device_folder + '/w1_slave'
+    lines = read_temp_raw(device_file)
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
@@ -20,8 +24,13 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        return temp_c
+        temp_formatted = "sensor " + str(bus_index + 1) + ": "  + str(temp_c)
+        return temp_formatted
 
 while True:
     print(read_temp())
     time.sleep(1)
+    if bus_index == 1:
+        bus_index = 0
+    else:
+        bus_index = 1
